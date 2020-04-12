8cf79fdfc6041029fc250a504187285027fabe00
kangangi <wanjikukangangi@gmail.com>
2020-04-12 20:24:28 +0300

    add changes to User Class

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,9 @@
 export class User {
+  constructor(public userName:string,
+     public bio:string, 
+     public repos: number,
+     public followers: number,
+     public following: number,
+     public creationDate: Date){
+  }
 }
